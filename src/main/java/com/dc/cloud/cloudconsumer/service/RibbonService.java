cab98ff517d8c906967d3b5bae6d379a69e90451
dc <2586515115@qq.com>
2019-11-05 15:30:07 +0800

    测试

--- src/main/java/com/dc/cloud/cloudconsumer/service/RibbonService.java
+++ src/main/java/com/dc/cloud/cloudconsumer/service/RibbonService.java
@@ -1,7 +1,6 @@
 package com.dc.cloud.cloudconsumer.service;
 
 import com.dc.cloud.cloudconsumer.vo.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
